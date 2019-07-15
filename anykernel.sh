f612810ee7b0e10f11d53d9b76b502ca24cdb7d8
Jago Gardiner (nysadev) <jagogardiner@gmail.com>
2019-07-15 13:01:12 +0100

    Treewide: Bring up for Acrux
    * AK3 modified for my own purposes.
    * Thanks A LOT to to @whoknowswhoiam - this might as well be a fork of his repository.
    * None of these credits should really be mine.
    
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,56 +4,68 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Acrux Kernel by nysadev
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=platina
+device.name2=MI8Lite
+device.name3=mi8lite
+device.name4=8lite
 device.name5=
-supported.versions=
+supported.versions=9
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+# Save the users from themselves
+android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
+supported_version=9;
+if [ "$android_version" != "$supported_version" ]; then
+  ui_print " "; ui_print "You are on $android_version but this kernel is only for $supported_version!";
+  exit 1;
+fi;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
+## begin vendor changes
+mount -o rw,remount -t auto /vendor >/dev/null;
+
+# Make a backup of init.target.rc
+restore_file /vendor/etc/init/hw/init.target.rc;
+backup_file /vendor/etc/init/hw/init.target.rc;
+
+# Make a backup of vendor build.prop
+restore_file /vendor/build.prop;
+backup_file /vendor/build.prop;
+
+# Add performance tweaks
+append_file /vendor/build.prop "AcruxBuildProps" build.prop
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
+# Add skip_override parameter to cmdline so user doesn't have to reflash Magisk
+if [ -d $ramdisk/.backup ]; then
+  ui_print " "; ui_print "Magisk detected! Patching cmdline so reflashing Magisk is not necessary...";
+  patch_cmdline "skip_override" "skip_override";
+else
+  patch_cmdline "skip_override" "";
+fi;
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+# Remove recovery service so that TWRP isn't overwritten
+remove_section init.rc "service flash_recovery" ""
 
 # end ramdisk changes
 
