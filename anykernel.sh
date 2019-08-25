004e3f8a6d45976bb62cebe90812adef523f6903
nysascape <nysadev@raphielgang.org>
2019-08-25 23:11:50 +0000

    anykernel: Cleanup
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Acrux Kernel by nysadev
+kernel.string=Acrux kernel by nysascape
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -25,14 +25,6 @@ ramdisk_compression=auto;
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
-# Save the users from themselves
-android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
-supported_version=9;
-if [ "$android_version" != "$supported_version" ]; then
-  ui_print " "; ui_print "You are on $android_version but this kernel is only for $supported_version!";
-  exit 1;
-fi;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
