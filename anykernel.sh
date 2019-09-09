89f570e130cb3de5ac15e5887da31dd63f7f95b6
nysascape <nysadev@raphielgang.org>
2019-09-09 00:48:16 +0000

    anykernel: Alcor HMP variant
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Alcor kernel by nysascape
+kernel.string=Alcor (HMP) kernel by nysascape
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
